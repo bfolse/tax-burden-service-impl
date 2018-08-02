8280106df196ec42c0c4d4f8441172ae30aad444
Brian Folse <bmfolse@gmail.com>
2018-08-02 14:21:07 -0400

    add IF EXISTS to SQL drop table statements

--- src/main/resources/schema-postgresql.sql
+++ src/main/resources/schema-postgresql.sql
@@ -8,13 +8,13 @@ DROP TABLE IF EXISTS public.consumer_expenditure_profile_entry;
 DROP TABLE IF EXISTS public.consumer_expenditure_profile CASCADE;
 
 -- Table: public.tax_definition_expenditure_category
-DROP TABLE public.tax_definition_expenditure_category;
+DROP TABLE IF EXISTS public.tax_definition_expenditure_category;
 
 -- Table: public.expenditure_category_group_expenditure_category_key
-DROP TABLE public.expenditure_category_group_expenditure_category_key;
+DROP TABLE IF EXISTS public.expenditure_category_group_expenditure_category_key;
 
 -- Table: public.expenditure_category_group
-DROP TABLE public.expenditure_category_group;
+DROP TABLE IF EXISTS public.expenditure_category_group;
 
  -- Table: public.tax_payer_profile_political_division_keys
 DROP TABLE IF EXISTS public.tax_payer_profile_political_division_keys;
@@ -44,7 +44,7 @@ DROP TABLE IF EXISTS public.assessed_rate;
 DROP TABLE IF EXISTS public.tax_rate CASCADE;
 
 -- Table: public.postal_code_political_division
-DROP TABLE public.postal_code_political_division;
+DROP TABLE IF EXISTS public.postal_code_political_division;
 
 -- Table: public.political_division
 DROP TABLE IF EXISTS public.political_division;
