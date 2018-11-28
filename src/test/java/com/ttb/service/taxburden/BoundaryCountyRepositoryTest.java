02b8762c80fad6094ee1b322d398ee92eaa5a9cb
Brian Folse <bmfolse@gmail.com>
2018-11-28 13:33:29 -0500

    attempt to unit test postgis with testcontainers

--- src/test/java/com/ttb/service/taxburden/BoundaryCountyRepositoryTest.java
+++ src/test/java/com/ttb/service/taxburden/BoundaryCountyRepositoryTest.java
@@ -9,8 +9,6 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.boot.test.util.TestPropertyValues;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.testcontainers.containers.PostgreSQLContainer;
@@ -21,29 +19,29 @@ import static org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
-//@ContextConfiguration(initializers = {BoundaryCountyRepositoryTest.Initializer.class})
+@ContextConfiguration(initializers = {BoundaryCountyRepositoryTest.Initializer.class})
 public class BoundaryCountyRepositoryTest {
-//    @ClassRule
-//    public static PostgreSQLContainer postgreSQLContainer =
-//            (PostgreSQLContainer) new PostgreSQLContainer("postgres:9.4")
-//                    .withDatabaseName("sampledb")
-//                    .withUsername("sampleuser")
-//                    .withPassword("samplepwd")
-//                    .withStartupTimeout(Duration.ofSeconds(600));
+    @ClassRule
+    public static PostgreSQLContainer postgreSQLContainer =
+            (PostgreSQLContainer) new PostgreSQLContainer("postgres:9.4")
+                    .withDatabaseName("sampledb")
+                    .withUsername("sampleuser")
+                    .withPassword("samplepwd")
+                    .withStartupTimeout(Duration.ofSeconds(600));
 
     @Autowired
     private BoundaryCountyRepository BoundaryCountyRepository;
 
-//    static class Initializer
-//            implements ApplicationContextInitializer<ConfigurableApplicationContext> {
-//        public void initialize(ConfigurableApplicationContext configurableApplicationContext) {
-//            TestPropertyValues.of(
-//                    "spring.datasource.url=" + postgreSQLContainer.getJdbcUrl(),
-//                    "spring.datasource.username=" + postgreSQLContainer.getUsername(),
-//                    "spring.datasource.password=" + postgreSQLContainer.getPassword()
-//            ).applyTo(configurableApplicationContext.getEnvironment());
-//        }
-//    }
+    static class Initializer
+            implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+        public void initialize(ConfigurableApplicationContext configurableApplicationContext) {
+            TestPropertyValues.of(
+                    "spring.datasource.url=" + postgreSQLContainer.getJdbcUrl(),
+                    "spring.datasource.username=" + postgreSQLContainer.getUsername(),
+                    "spring.datasource.password=" + postgreSQLContainer.getPassword()
+            ).applyTo(configurableApplicationContext.getEnvironment());
+        }
+    }
 
     @Test
     public void findAllTest() {
